55dad87d90e1fc0677c00e9ffc4b262c853eef05
Simon Bohlin <simon.bohlin@gmail.com>
2010-05-11 03:54:08 +0200

    Initial complete commit -- no file ignored...

---
+++ FolderdateFollowsFiles/Follower.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Follower
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.sFolder = New System.Windows.Forms.TextBox
+        Me.lbl = New System.Windows.Forms.Label
+        Me.tbStatus = New System.Windows.Forms.TextBox
+        Me.btnGo = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'sFolder
+        '
+        Me.sFolder.Location = New System.Drawing.Point(52, 9)
+        Me.sFolder.Name = "sFolder"
+        Me.sFolder.Size = New System.Drawing.Size(161, 20)
+        Me.sFolder.TabIndex = 0
+        '
+        'lbl
+        '
+        Me.lbl.AutoSize = True
+        Me.lbl.Location = New System.Drawing.Point(7, 12)
+        Me.lbl.Name = "lbl"
+        Me.lbl.Size = New System.Drawing.Size(39, 13)
+        Me.lbl.TabIndex = 1
+        Me.lbl.Text = "Folder:"
+        '
+        'tbStatus
+        '
+        Me.tbStatus.Location = New System.Drawing.Point(11, 35)
+        Me.tbStatus.Multiline = True
+        Me.tbStatus.Name = "tbStatus"
+        Me.tbStatus.Size = New System.Drawing.Size(260, 200)
+        Me.tbStatus.TabIndex = 2
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(219, 9)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(52, 20)
+        Me.btnGo.TabIndex = 3
+        Me.btnGo.Text = "Follow"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'Follower
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(283, 239)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.tbStatus)
+        Me.Controls.Add(Me.lbl)
+        Me.Controls.Add(Me.sFolder)
+        Me.Name = "Follower"
+        Me.Text = "Follower"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents sFolder As System.Windows.Forms.TextBox
+    Friend WithEvents lbl As System.Windows.Forms.Label
+    Friend WithEvents tbStatus As System.Windows.Forms.TextBox
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+
+End Class
---
+++ FolderdateFollowsFiles/Follower.vb
@@ -0,0 +1,142 @@
+﻿Imports System.Diagnostics
+Imports System.Security.Permissions
+Imports System.IO
+
+Public Class Follower
+    'testning: ~S\temp\T*\w2k\*.bat
+
+    Public Shared Sub addLog(ByVal txt As String)
+        Follower.tbStatus.Text = txt & vbCrLf & Follower.tbStatus.Text
+        Follower.tbStatus.Update()
+        'System.Windows.Forms.Application.DoEvents()
+    End Sub
+
+    Public Shared Sub addTodo(ByVal txt As String)
+        Follower.timer.Stop()    'ensure that the timer won't TICK immediately. This also ensures we modify the directories after file reset + write (2 operations) has finished.
+        Follower.timer.Interval = Follower.shortInterval
+
+        Follower.todo_list.Add(txt)
+        Follower.todo.Enqueue(txt)
+
+        Debug.Print("added " + txt + ". N:=" + Follower.todo_list.Count.ToString)
+
+        Follower.timer.Start()
+    End Sub
+
+    Public Shared todo_list As New HashSet(Of String)
+    Public Shared todo As New System.Collections.Generic.Queue(Of String)
+    Private timer As New Timer
+
+    'In live system, set the short interval to more than a slow/big file create + write operation. File transfer via VPN is sometimes SLOOOOW.
+    Public shortInterval As Integer = 3000
+    Public longInterval As Integer = 10000
+
+    'http://msdn.microsoft.com/en-us/library/system.io.filesystemwatcher.aspx
+    Private instance As New System.IO.FileSystemWatcher
+
+    Private Sub sFolder_MouseDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles sFolder.MouseDoubleClick
+        Me.sFolder.Text = "C:\Users\S"
+    End Sub
+
+    ' Define the event handlers.
+    Private Shared Sub OnChanged(ByVal source As Object, ByVal e As FileSystemEventArgs)
+        ' Specify what is done when a file is changed, created, or deleted.
+        Dim msg = ("File: " & e.FullPath & " " & e.ChangeType)
+        Follower.addLog(msg)
+        Follower.addTodo(e.FullPath)
+        System.Windows.Forms.Application.DoEvents()
+        Debug.Print(msg)
+    End Sub
+
+    Public Shared Sub OnTick()
+        'Betar av todo_list.
+        'TODO:  Vid fel, lägg till sist i listan och vänta 1 minut. Sedan upprepar den framifrån, så den fördröjda hamnar sist.
+        '       Eventuellt måste de som ger fel flyttas till todo_1fel, sedan till todo_2fel, sedan kastas bort oåtgärdade.
+
+        Debug.Print("tick. N=" + Follower.todo.Count.ToString)
+        While Follower.todo.Count > 0
+            Dim x
+            REM x = Follower.todo_list.First(): Follower.todo_list.Remove(x)
+            x = Follower.todo.Dequeue()
+
+            'Hoppa över x ifall det finns fler kö-rader med nyare datum. (Undviker dubbeljobb)
+            While Follower.todo.Contains(x)
+                x = Follower.todo.Dequeue()
+            End While
+
+            If Not Follower.updateFolders(x) Then
+                'TODO: add failures to secondary todo_list?
+            End If
+            Debug.Print("tOck. N=" + Follower.todo.Count.ToString)
+        End While
+
+        'Nothing more to do (since count is now 0)
+        Follower.timer.Interval = Follower.longInterval
+    End Sub
+
+    Public Shared Function updateFolders(ByVal file_path As String) As Boolean
+        If Not My.Computer.FileSystem.FileExists(file_path) Then
+            If My.Computer.FileSystem.DirectoryExists(file_path) Then Debug.Print("Directory!!!")
+            Return True  'eliminate from hash, even though it failed
+        End If
+
+        Dim file_time = My.Computer.FileSystem.GetFileInfo(file_path).LastWriteTime
+        Dim result As Boolean = False
+
+        Dim dir_path = My.Computer.FileSystem.GetParentPath(file_path)
+        While dir_path.Length > Follower.sFolder.Text.Length
+            Dim dir_info = My.Computer.FileSystem.GetDirectoryInfo(dir_path)
+            Dim timediff = (file_time - dir_info.LastWriteTime)
+            If timediff.TotalSeconds > 5 Then
+                Debug.Print("t_time=" + file_time.ToString + ", dir_time=" + dir_info.LastWriteTime.ToString + " -- diff (sec): " + (file_time - dir_info.LastWriteTime).TotalSeconds.ToString)
+                Try
+                    dir_info.LastWriteTime = file_time
+                    Debug.Print("Updated " + dir_path + " to " + file_time.ToString)
+                Catch ex As Exception
+                    result = False
+                    Debug.Print("FAILED to update " + dir_path + " to " + file_time.ToString)
+                End Try
+
+                'Dim dir_name = My.Computer.FileSystem.GetName(dir_path)
+                'My.Computer.FileSystem.RenameDirectory(dir_path, "$" + dir_name + "$")
+                'My.Computer.FileSystem.RenameDirectory(dir_path.Replace("\" + dir_name, "\$" + dir_name + "$"), dir_name)
+            End If
+
+            'To be used when IF is changed into WHILE:
+            If dir_path.Length > 4 Then dir_path = My.Computer.FileSystem.GetParentPath(dir_path)
+            Debug.Print("next folder became " + dir_path)
+        End While
+        Return result
+    End Function
+
+    Private Sub Follower_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        addLog("Enter a folder path above")
+
+        ' Prepare timer that can handle updating folders (seems it must be done outside of the OnChange event!?)
+        timer.Interval = Me.longInterval
+        AddHandler timer.Tick, AddressOf OnTick
+        timer.Enabled = True
+
+        ' Add event handlers.
+        AddHandler instance.Changed, AddressOf OnChanged
+    End Sub
+
+    Private Sub btnGo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnGo.Click
+        Dim t As String
+        t = sFolder.Text
+        If Len(t) > 5 And My.Computer.FileSystem.DirectoryExists(t) Then
+            addLog("Following: " & t)
+            instance.Path = t
+            instance.Filter = "*.*"
+            'instance.NotifyFilter = NotifyFilters.LastWrite
+            instance.NotifyFilter = (NotifyFilters.LastAccess Or NotifyFilters.LastWrite Or NotifyFilters.FileName Or NotifyFilters.DirectoryName)
+            instance.IncludeSubdirectories = True
+
+            ' Begin watching.
+            instance.EnableRaisingEvents = True
+        Else
+            instance.EnableRaisingEvents = False
+            addLog("Stopped")
+        End If
+    End Sub
+End Class
